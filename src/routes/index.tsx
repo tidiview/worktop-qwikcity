3764c243161d937481514c18f1ac4200265a5f06
tidiview <francois.vidit@laposte.net>
2022-11-16 21:08:00 +0100

    upgrade to latest version of dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,7 @@ export default component$(() => {
       <ul>
         <li>This homepage uses a layout without a menu.</li>
         <li><ruby>怜<rp>（</rp><rt>れ</rt><rp>）</rp>央<rp>（</rp><rt>お</rt><rp>）</rp></ruby>。これは日本語の文書のフォントのサンプルです。</li>
+        <li><ruby lang="fr">François<rp>（</rp><rt lang="ja">フランソワ</rt><rp>）</rp>&nbsp;<rp>（</rp><rt lang="ja">・</rt><rp>）</rp>VIDIT<rp>（</rp><rt lang="ja">ヴィディ</rt><rp>）</rp></ruby>。</li>
         <li>
           <span>The </span>
           <a href="/docs">Documentation</a>
